bb134511f2f89966d218ab221ab3cf28559d8486
Bdoherty1 <bdoherty988@gmail.com>
2024-02-28 17:41:25 -0600

    generate markdown for README, also Handled scenario where no license is specified; functions now return empty strings for badge, link, and section generation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,63 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
+function renderLicenseBadge(license) {
+    if (license) {
+      return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](LICENSE)`;
+    }
+    return '';
+  }
+  
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license) {
+      return `[License](#license)`;
+    }
+    return '';
+  }
+  
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license) {
+      return `## License
+  
+  This application is covered under the ${license} license.
+  
+  For more details, please see [LICENSE](LICENSE).`;
+    }
+    return '`# ${data.title}';
+  }
+  
+  // generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.title}
+    ## Description
+    ${data.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    ${data.installation}
+    
+    ## Usage
+    ${data.usage}
+    
+    ${renderLicenseSection(data.license)}
+    
+    ## Contributing
+    ${data.contributing}
+    
+    ## Tests
+    ${data.tests}
+    
+    ## Questions
+    - GitHub: [${data.username}](https://github.com/${data.username})
+    - Email: ${data.email}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
